f6f54370ade274b27e0a04c51b82e8eb6bc47ca3
Fhwang <hdh0926@naver.com>
2023-03-01 16:15:01 +0900

    fix: pull request merge

--- test/images/inspect.js
+++ test/images/inspect.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const Code    = require('code');
 const Lab     = require('lab');
 const harness = require('../harness');
 
@@ -14,14 +13,9 @@ lab.experiment('images - inspect', () => {
       .get('/images/hello-world/json')
       .reply(200, {});
 
-    harness.client.images().inspect('hello-world').then(() => {
-      Code.expect(scope.isDone()).to.equal(true);
-    }, () => {
-      Code.fail('should be a 200 response');
-    }).finally(() => {
-      harness.clean();
-      done();
-    });
+    const req = harness.client.images().inspect('hello-world');
+
+    harness.handleSuccess(scope, 200, req, done);
 
   });
 
--- test/images/search.js
+++ test/images/search.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const Code    = require('code');
 const Lab     = require('lab');
 const harness = require('../harness');
 
@@ -12,17 +11,14 @@ lab.experiment('images - search', () => {
 
     const scope = harness.mock()
       .get('/images/search')
-      .query({term: 'hello-world'})
+      .query({ term: 'hello-world' })
       .reply(200, {});
 
-    harness.client.images().search({term: 'hello-world'}).then(() => {
-      Code.expect(scope.isDone()).to.equal(true);
-    }, () => {
-      Code.fail('should be a 200 response');
-    }).finally(() => {
-      harness.clean();
-      done();
+    const req = harness.client.images().search({
+      term: 'hello-world'
     });
+
+    harness.handleSuccess(scope, 200, req, done);
 
   });
 
