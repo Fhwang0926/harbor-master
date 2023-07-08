69dbc2fa29cf5d649c7cc6513c212012cd1a1ca5
Fhwang <hdh0926@naver.com>
2023-07-09 01:32:26 +0900

    fix

--- util/index.js
+++ util/index.js
@@ -1,33 +1,64 @@
 const docker = require("../index");
 
 const client = docker.Client({
-  host: "console.3vi.one",
-  port: 65533,
+  host: "localhost",
+  port: 2375,
   axios: true,
 });
 
 let run = async () => {
   try {
     let info = await client.containers().list({
-      filters: {
-        Name: ['test']
-      }
+      // filters: {
+      //   Name: ['test']
+      // }
     });
 
     for (let i = 0; i < info.length; i++) {
       // get
       let con = await client.containers().inspect(info[i].Id, { size: false });
-      console.log("con", con.SizeRw);
+      console.log("con", con);
 
-      if(info[i].Names[info[i].Names.length - 1].replace('/', '') != 'test') {
-        continue
-      }
+      let rv = await client.exec().create(con.Id, {
+        "AttachStdin": false,
+        "AttachStdout": true,
+        "AttachStderr": true,
+        "DetachKeys": "ctrl-p,ctrl-q",
+        "Tty": true,
+        "Cmd": [
+          "bash",
+          "-c",
+          "echo aa=1 > ~/aa.txt"
+        ],
+        "Env": [
+          "FOO=bar",
+          "BAZ=quux"
+        ]
+      })
 
-      let rv = ''
+      console.log(rv)
+
+
+      let rv_start = await client.exec().start(rv.Id, {
+        "Detach": false,
+        "Tty": false,
+        "ConsoleSize": [
+        80,
+        64
+        ]
+      })
+
+      console.log(rv_start)
+
+      // if(info[i].Names[info[i].Names.length - 1].replace('/', '') != 'test') {
+      //   continue
+      // }
+
+      // let rv = ''
 
       // test
-      rv = await client.networks().disconnect('7583571.1.5.1683988549065', { Container: 'test', Force: false });
-      console.log('post', 'disconnect', rv)
+      // rv = await client.networks().disconnect('7583571.1.5.1683988549065', { Container: 'test', Force: false });
+      // console.log('post', 'disconnect', rv)
 
       // post
       // rv = await client.containers().stop(info[i].Names[info[i].Names.length - 1].replace('/', ''));
@@ -41,7 +72,7 @@ let run = async () => {
       // console.log('post', 'start', rv)
     }
   } catch (e) {
-    console.log(e);
+    console.log(e.body);
   }
 };
 
